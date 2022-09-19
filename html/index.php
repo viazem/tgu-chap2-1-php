39a9428f52a1afeae01a4d2b25b20af1017d8ed4
viazem <viazem@gmail.com>
2022-09-19 21:38:43 +0300

    Полный код для запуска PHP

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+	<meta charset="utf-8">
+	<title>Моя первая программа</title>
+	<?php
+	    error_reporting(E_ALL);
+	    ini_set('display_errors', 'on');
+	    mb_internal_encoding('UTF-8');
+	?>
+    </head>
+    <body>
+	<?php
+	    echo 'Моя первая программа';
+	?>
+    </body>
+</html>
