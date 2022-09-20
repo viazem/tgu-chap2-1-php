54339296a5359317206830704b5a3c666dd108a6
viazem <viazem@gmail.com>
2022-09-20 18:55:40 +0300

    Раздел 2. Переменные в PHP

--- html/code.php
+++ html/code.php
@@ -1,7 +1,39 @@
 <?php
-    echo 'привет мир'; // коментарий
+    
+    $_br = '<br>';
+
+    //echo 'привет мир'; // коментарий
     /*
-    Многострочный комментарий
+    Многострочный
+    комментарий
     */
-    echo 'hello';
+    //echo 'hello';
+    
+    $a = 1 + 2; echo $a;echo $_br;
+    $b = 3 - 2; echo $b;echo $_br;
+    $c = 3 * 2; echo $c;echo $_br;
+    $d = 4 / 2; echo $d;echo $_br;
+    
+    $a = 1 + 2 + 3; echo $a;echo $_br;
+    
+    $a = 1;
+    $b = 2;
+    echo $a + $b;echo $_br;
+    
+    $c = $a + $b;echo $c;echo $_br;
+    
+    $a = 10;
+    $b = 2;
+    
+    echo $a + $b;echo $_br;
+    echo $a - $b;echo $_br;
+    echo $a * $b;echo $_br;
+    echo $a % $b;echo $_br;
+    
+    echo $_br;echo $_br;
+    
+    $c = 10; $d = 5; $result = $c + $d; echo $result;echo $_br;
+    
+    echo $_br;
+    
 ?>
--- html/index.php
+++ html/index.php
@@ -2,12 +2,12 @@
 <html>
     <head>
 	<meta charset="utf-8">
-	<title>Моя первая программа</title>
+	<title>"Моя первая программа"</title>
 	<?php
 	    error_reporting(E_ALL);
 	    ini_set('display_errors', 'on');
 	    mb_internal_encoding('UTF-8');
-	    
+	
 	    include 'code.php';
 	?>
     </head>
